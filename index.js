acdcb056404e4fd2ac9f79ae1f55537f54e00fbb
nizhammm <nizhammustaqim@gmail.com>
2022-02-10 12:03:28 +0700

    practice filteredData

---
+++ index.js
@@ -0,0 +1,47 @@
+// 
+const books = [
+    "LOTR",
+    "Harry Potter",
+    "Doraemon",
+    "Naruto"
+]
+
+const duplicateForEach = (arr, callback) => {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i, arr)
+    }
+}
+
+// duplicateForEach(books, (val, index) => {
+//     console.log(val)
+// })
+
+
+
+// Goals
+// 1. Duplikat method filter
+// 2. Filter akan menghasilkan sebuah array -> pasti akan return array
+// 3. Gimana cara menentukan item yang akan msk ke array ?
+//      Callback function harus menghasilkan true
+// 4. Gimana cara masukkan item ke dalam array ?
+//      Gunakan method push
+
+const duplicateFilter = (arr, callback) => {
+    let resultArr = [];
+    for (let i = 0; i < arr.length; i++){
+        if(callback(arr[i], i, arr)) {
+            resultArr.push(arr[i])
+        }
+    }
+
+    return resultArr;
+}
+let filteredData = duplicateFilter (books, (val) => {
+    return val.length > 6
+})
+
+// let filteredData = books.filter((val) => {
+//     return val.length > 6
+// })
+
+console.log (filteredData)
